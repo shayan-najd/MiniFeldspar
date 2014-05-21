905db0051ee1d41983070a29d8768972178f6179
Shayan Najd <sh.najd@gmail.com>
2014-05-21 13:30:25 +0100

    Full automation of the experiment

--- Shayan/Experiment/Nrm/Time/Script.hs
+++ Shayan/Experiment/Nrm/Time/Script.hs
@@ -33,4 +33,4 @@ wordsWhen     :: (Char -> Bool) -> String -> [String]
 wordsWhen p s =  case dropWhile p s of
                       "" -> []
                       s' -> w : wordsWhen p s''
-                            where (w, s'') = break p s'
+                                    where (w, s'') = break p s'
---
+++ Shayan/Process.hs
@@ -0,0 +1,18 @@
+import System.Directory
+import System.Environment
+
+(</>) :: String -> String -> String
+d </> f = d ++ "/" ++ f
+
+main :: IO()
+main = do [d] <- getArgs
+          fs <- fmap (filter ((== "time") . take 4))
+                $ getDirectoryContents d
+          avgs <- mapM (\ f -> do 
+                          fc <- readFile (d </> f)
+                          let ls = lines fc
+                          return (drop 4 f 
+                                 , sum (map (read :: String -> Float) ls) 
+                                           / fromIntegral (length ls))
+                       ) fs
+          writeFile (d </> "result") (show avgs)        
