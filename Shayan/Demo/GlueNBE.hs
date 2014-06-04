557dcd5e9e63765ffafb7b6d3fd041bdeab97825
Shayan Najd <sh.najd@gmail.com>
2014-06-04 10:05:13 +0100

    Docs

---
+++ Shayan/Demo/GlueNBE.hs
@@ -0,0 +1,55 @@
+-- Inspired by Dybjer and Flinisky's 
+module Demo.GlueNBE where
+
+import Data.Maybe
+
+data Syn = S | K | App Syn Syn | Zero | Succ | Rec
+
+primRec :: a -> (Int -> a -> a) -> Int -> a
+primRec z _ 0 = z
+primRec z s n = s (n - 1) (primRec z s (n - 1))
+ 
+data Sem  = Fun Syn (Sem -> Maybe Sem) | Nat Int
+
+reifyNat :: Int -> Syn
+reifyNat 0 = Zero
+reifyNat n = App Succ (reifyNat (n-1))
+
+reify :: Sem -> Syn
+reify (Fun syn _) = syn
+reify (Nat n)     = reifyNat n
+
+appSem :: Sem -> Sem -> Maybe Sem
+appSem (Fun _ f) arg = f arg
+appSem (Nat _)   _   = Nothing     
+
+succSem :: Sem -> Maybe Sem
+succSem (Fun _ _) = Nothing
+succSem (Nat n)   = Just (Nat (n + 1)) 
+
+recSem :: a -> (Int -> a -> a) -> Sem -> Maybe a
+recSem _ _ (Fun _ _) = Nothing   
+recSem z s (Nat n)   = Just (primRec z s n)
+
+eval :: Syn -> Maybe Sem
+eval S           = Just (Fun S (\ f -> 
+                     Just (Fun (App S (reify f)) (\ g -> 
+                       Just (Fun (App (App S (reify f)) (reify g)) (\ x -> 
+                         do f' <- appSem f x
+                            g' <- appSem g x
+                            appSem f' g'))))))
+eval K           = Just (Fun K (\ x -> 
+                     Just (Fun (App K (reify x)) (\ _y -> Just x))))
+eval (App e0 e1) = do e0' <- eval e0 
+                      e1' <- eval e1
+                      appSem e0' e1'
+eval Zero        = Just (Nat 0) 
+eval Succ        = Just (Fun Succ succSem)
+eval Rec         = Just (Fun Rec (\ z -> 
+                     Just (Fun (App Rec (reify z)) (\ s -> 
+                       Just (Fun (App (App Rec (reify z)) (reify s)) 
+                         (recSem z (\ n c -> fromJust (do asn <- appSem s (Nat n)
+                                                          appSem asn c)
+                                   )))))))
+
+
---
+++ Shayan/Demo/TypeDirectedNBE.hs
@@ -0,0 +1,55 @@
+-- Inspired by Dybjer and Flinisky's 
+module Demo.TypeDirectedNBE where
+
+import Prelude ()
+import MyPrelude
+
+import Environment.Map
+
+data Exp = Var String | Abs String Exp | App Exp Exp
+
+deriving instance Show Exp
+deriving instance Eq Exp
+
+data Typ = Base String | Arrow Typ Typ
+
+type Unq a = State Int a
+
+newVar :: Unq String
+newVar = do i <- getState
+            put (i + 1)
+            return ("x" ++ show i)    
+  
+data Sem = TM (Unq Exp) | Fun (Monad m => Sem -> m Sem)
+
+eval :: Monad m => Exp -> Env String Sem -> m Sem
+eval (Var x)     r = get x r
+eval (Abs x t)   r = return (Fun (\ v -> eval t ((x , v) : r)))
+eval (App e1 e2) r = do Fun f <- eval e1 r
+                        a     <- eval e2 r  
+                        f a
+
+reify :: Typ -> Sem -> Unq Exp
+reify (Base _)      (TM e)  = e 
+reify (Arrow t1 t2) (Fun f) = do v <- newVar
+                                 Abs v <$> (do e' <- f (reflect t1 
+                                                        (return (Var v)))
+                                               reify t2 e')
+reify _             _       = fail "Bad Type!"
+
+reflect :: Typ -> Unq Exp -> Sem
+reflect (Base _)      e = TM e
+reflect (Arrow t1 t2) e = Fun (\ a -> return 
+                                      (reflect t2 (App <$> e <*> reify t1 a)))
+
+norm :: Typ -> Exp -> Exp
+norm t e = evalState (do r <- eval e []
+                         reify t r) 0 
+
+test :: Bool
+test = norm (Arrow (Arrow (Base "a") (Base "b")) (Arrow (Base "a") (Base "b")))
+       (Abs "f" 
+        (Abs "x" 
+         (App (Abs "y" (App (Var "f") (Var "y"))) 
+          (App (Var "f") (Var "x"))))) == 
+       (Abs "x0" (Abs "x1" (App (Var "x0") (App (Var "x0") (Var "x1")))))
