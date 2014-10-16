724015405f62cb4c4aad91b860b9f8d181ea253b
Sam Lindley <Sam.Lindley@ed.ac.uk>
2014-10-17 00:09:14 +0100

    Revert "spell out syntactic sugar for lambda"
    This reverts commit 43ff7cfea74b9435b30b0fd36bbe2d9c586ab7fa.

--- Philip/QDsl/qdsl.hs
+++ Philip/QDsl/qdsl.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TemplateHaskell, GADTs #-}
-
 module TemplateHaskell where
 
 import Language.Haskell.TH.Syntax
@@ -26,18 +24,8 @@ forLoop = [|| \ n s0 b -> snd (while (\ (i , _s) -> i < n)
                                      (\ (i , s ) -> (i + 1 , b i s))   
                                      (0 , s0)) ||]          
 
-forLoop' :: FO s => Int -> s -> (Int -> s -> s) -> s
-forLoop' n s0 b = snd (while (\ (i , _s) -> i < n) 
-                             (\ (i , s ) -> (i + 1 , b i s))   
-                             (0 , s0))
-
-
 ex1 :: Qu Int          
 ex1 = [|| $$forLoop 10 0 (\ i s -> i + s) ||]
-
-ex1' :: Qu Int          
-ex1' = [|| forLoop' 10 0 (\ i s -> i + s) ||]
-
           
 data Vector a where 
   Indexed :: Int -> (Int -> a) -> Vector a           
@@ -48,45 +36,23 @@ instance Functor Vector where
 zipWithVec :: Qu ((a -> b -> c) -> Vector a -> Vector b -> Vector c)
 zipWithVec = [|| \ f (Indexed m g) (Indexed n h) -> 
                  Indexed (min m n) (\ i -> f (g i) (h i)) ||]
-
-zipWithVec' :: (a -> b -> c) -> Vector a -> Vector b -> Vector c
-zipWithVec' f (Indexed m g) (Indexed n h) = Indexed (min m n) (\ i -> f (g i) (h i))
              
 sumVec ::(FO a , Num a) => Qu (Vector a -> a)             
 sumVec = [|| \ (Indexed n f) -> $$forLoop n 0 (\ i s -> s + (f i)) ||]
 
-sumVec' ::(FO a , Num a) => Vector a -> a
-sumVec' (Indexed n f) = forLoop' n 0 (\ i s -> s + (f i))
-
-
 scalarProd :: (FO a , Num a) => Qu (Vector a -> Vector a -> a) 
 scalarProd = [|| \ a b -> $$sumVec ($$zipWithVec (*) a b) ||] 
-
-scalarProd' :: (FO a , Num a) => Vector a -> Vector a -> a
-scalarProd' a b = sumVec' (zipWithVec' (*) a b)
 
 idVec :: Qu (Int -> Vector Int)
 idVec = [|| \ n -> Indexed n (\ i -> i) ||]
 
-idVec' :: Int -> Vector Int
-idVec' n = Indexed n (\ i -> i)
-
 ex2 :: Qu Int
 ex2 = [|| $$scalarProd ($$idVec 10) (fmap (* 2) ($$idVec 10)) ||]
-
-ex2' :: Qu Int
-ex2' = [|| scalarProd' (idVec' 10) (fmap (* 2) (idVec' 10)) ||]
 
 memorize :: Qu (Vector a -> Vector a)
 memorize = [|| \ (Indexed n f) -> let a = arr n f in Indexed n (\ i -> a ! i) ||]  
 
-memorize' :: Vector a -> Vector a
-memorize' (Indexed n f) = let a = arr n f in Indexed n (\ i -> a ! i)
-
 ex3 :: Qu Int
 ex3 = [|| let vec = $$memorize ($$idVec 10)
           in  $$scalarProd vec (fmap (* 2) vec) ||]
-
-ex3' :: Qu Int
-ex3' = [|| let vec = memorize' (idVec' 10)
-           in  scalarProd' vec (fmap (* 2) vec) ||]
+          
